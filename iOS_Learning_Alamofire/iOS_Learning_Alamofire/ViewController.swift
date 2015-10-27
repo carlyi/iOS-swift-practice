23f5326d6c08fe3b78ddb604c2c3055bfe6ea776
carlyi <yiyuanping1234@qq.com>
2015-10-26 19:46:43 -0700

    Create Project to Learn usinge Alamofire.

---
+++ iOS_Learning_Alamofire/iOS_Learning_Alamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Learning_Alamofire
+//
+//  Created by  义远平 on 15/10/26.
+//  Copyright (c) 2015年 Carl Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
